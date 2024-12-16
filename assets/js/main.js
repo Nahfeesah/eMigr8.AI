8110187b692b960fd7ea67dffbb2c82fc3da4341
Nofisat Ibrahim <inofisat5@gmail.com>
2024-12-16 09:35:32 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,13 +128,5 @@
   window.onload = init;
 
 
-  /**
-   * Frequently Asked Questions Toggle
-   */
-  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
-    faqItem.addEventListener('click', () => {
-      faqItem.parentNode.classList.toggle('faq-active');
-    });
-  });
 
 })();
